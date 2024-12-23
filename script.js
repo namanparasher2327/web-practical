02883e67ff3faf8d6533d1e25b0b2fe6e50ea64f
namanparasher2327 <naman.2327cseai1015@kiet.edu>
2024-12-24 01:03:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Scroll to the menu section when the button is clicked
+function scrollToMenu() {
+    document.getElementById('menu').scrollIntoView({ behavior: 'smooth' });
+}
+
+// Contact form
